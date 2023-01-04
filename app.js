4a37b6a18646d34fb98ce3c5442a471933c0a754
Irandom <77416026+Irandomuno@users.noreply.github.com>
2023-01-03 23:45:11 -0800

    rearranging code and fixing functions

---
+++ app.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function  playRound(playerSelection, computerSelection) {
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+    }
+
+}
