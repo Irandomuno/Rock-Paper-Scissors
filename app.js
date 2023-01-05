8c689b2c787c47412bf13c3737859f97a776b41f
Irandom <77416026+Irandomuno@users.noreply.github.com>
2023-01-04 16:05:04 -0800

    fixed the last game function, giving back winners, draw, loss

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let compScore = 0;
+
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -9,17 +13,27 @@ function  playRound(playerSelection, computerSelection) {
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
                (playerSelection === "paper" && computerSelection === "rock") ||
                (playerSelection === "scissors" && computerSelection === "paper")) {
+        playerScore++;
         return `You Win! ${playerSelection} beats ${computerSelection}`;        
     } else {
+        compScore++;
         return `You Lost! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
 function game() {
-    const playerInput = playerSelection.toLowerCase();
-    const computerSelection = getComputerChoice();
     for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Choose your weapon", "rock, paper, or scissors").toLocaleLowerCase();
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
 
+        if (playerSelection > computerSelection) {
+            return "You beat the computer!";
+        } else if (playerSelection < computerSelection) {
+            return "You got beat by the computer!";
+        } else {
+            return "Wow! what a match, you both tied!";
+        }
     }
-
 }
+console.log(game());
